d7d6d37f3e8d669e985eba522ccb31fa780df2ea
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-18 14:57:38 +1200

    add backspace functionality

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -19,6 +19,12 @@ const useWordle = (solution) => {
   //handle keyup event & track current guess
   //if user presses enter, add the new guess
   const handleKeyup = ({ key }) => {
+    if (key === 'Backspace') {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1)
+      })
+      return
+    }
     //regex to ensure the if block only occurs if a user types a letter key
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
