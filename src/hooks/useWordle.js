7119249e12e3f627aa7fb7e3f2281729e011f53e
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-18 14:28:33 +1200

    log event listener keyup to console

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const useWorlde = (solution) => {
+const useWordle = (solution) => {
   const [turn, setTurn] = useState(0)
   const [currentGuess, setCurrentGuess] = useState('')
   const [guesses, setGuesses] = useState([]) // each guess is an array
@@ -18,9 +18,11 @@ const useWorlde = (solution) => {
 
   //handle keyup event & track current guess
   //if user presses enter, add the new guess
-  const handleKeyup = () => {}
+  const handleKeyup = ({ key }) => {
+    console.log(key)
+  }
 
   return { turn, currentGuess, guesses, isCorrect, handleKeyup }
 }
 
-export default useWorlde
+export default useWordle
