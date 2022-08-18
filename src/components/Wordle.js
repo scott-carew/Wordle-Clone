0a0a7d1f258914a035155f7cd660a6314cbb2791
Scott Carew <103097506+scott-carew@users.noreply.github.com>
2022-08-18 19:33:06 +1200

    add end of game functionality to wordle.js

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -10,12 +10,18 @@ export default function Wordle({ solution }) {
   useEffect(() => {
     window.addEventListener('keyup', handleKeyup)
 
-    return () => window.removeEventListener('keyup', handleKeyup)
-  }, [handleKeyup])
+    if (isCorrect) {
+      console.log('congrats, you win!')
+      window.removeEventListener('keyup', handleKeyup)
+    }
 
-  useEffect(() => {
-    console.log(guesses, turn, isCorrect)
-  }, [guesses, turn, isCorrect])
+    if (turn > 5) {
+      console.log('unlucky, out of guesses')
+      window.removeEventListener('keyup', handleKeyup)
+    }
+
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup, isCorrect, turn])
 
   return (
     <>
